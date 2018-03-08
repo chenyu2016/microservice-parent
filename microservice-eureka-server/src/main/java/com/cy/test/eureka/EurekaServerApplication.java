c6fb8e3349f5584e724e919503dee9b0cfac6732
陈禹 <chenyu@chenyu-mac-book.local>
2018-03-08 10:05:12 +0800

    Initial commit

---
+++ microservice-eureka-server/src/main/java/com/cy/test/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.cy.test.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ChenYu
+ * Date: 2018/3/6
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
